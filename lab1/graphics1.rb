517f14cf1d5ce8e61cd012770781c8cae6474510
Dmitry A. Ustalov <eveeel@gmail.com>
2009-12-21 23:40:30 +0500

    first lab is almost ready

--- lab1/graphics1.rb
+++ lab1/graphics1.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'singleton'
 require 'gl'
 require 'glu'
 require 'glut'
@@ -10,13 +11,17 @@ include Glu
 include Glut
 
 class Lab
+  include Singleton
   attr_reader :scene
 
   def initialize
     @zoom = 1.0
     @shift_x = 0.0
     @shift_y = 0.0
-    @rotate = 0
+    @rotate_x = 0.0
+    @rotate_y = 0.0
+    @rotate_z = 0.0
+    @trans = -1.0
 
     # Load and prepare verteces
     raw = YAML.load_file('scene.yaml')
@@ -34,7 +39,7 @@ class Lab
     glutReshapeFunc(method(:reshape).to_proc);
     glutIdleFunc(method(:idle).to_proc);
     glutKeyboardFunc(method(:keyboard).to_proc);
-    init_gl_window(640, 480)
+    init_gl_window(800, 600)
     glutMainLoop();
   end
 
@@ -47,23 +52,35 @@ class Lab
     glDepthFunc(GL_LEQUAL);
     # Enable depth testing
     glEnable(GL_DEPTH_TEST);
-    # Enable smooth color shading
-    glShadeModel(GL_SMOOTH);
+  end
+
+  def reshape w, h
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40.0, w / h, 1.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslate(0.0, 0.0, @trans);
   end
 
   def draw_gl_scene
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     # Reset the view
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity
+    #glMatrixMode(GL_MODELVIEW)
+    #glLoadIdentity();
+    glPushMatrix();
+    glTranslate(0.0, 0.0, @trans);
 
     # Translate scene
-    glTranslatef(@shift_x, @shift_y, 0.0)
+    glTranslatef(@shift_x, @shift_y, 0)
     glScalef(@zoom, @zoom, 1.0)
 
-    # Rotate scene on the Z-axis
-    glRotatef(@rotate, 0.0, 0.0, 1.0)
+    # Rotate scene
+    glRotatef(@rotate_x, 1.0, 0.0, 0.0)
+    glRotatef(@rotate_y, 0.0, 1.0, 0.0)
+    glRotatef(@rotate_z, 0.0, 0.0, 1.0)
 
     # Draw scene
     glBegin(GL_LINES)
@@ -74,21 +91,9 @@ class Lab
         glVertex4fv(v2);
       end
     glEnd
+    glPopMatrix();
 
     glutSwapBuffers;
-  end
-
-  def reshape w, h
-    h = 1 if h == 0
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (w <= h)
-      glOrtho(-1.5, 1.5, -1.5 * h/w, 1.5 * h/w, -10.0, 10.0);
-    else
-      glOrtho(-1.5 * w/h, 1.5 * w/h, -1.5, 1.5, -10.0, 10.0);
-    end
-    glMatrixMode(GL_MODELVIEW);
   end
 
   def idle
@@ -102,24 +107,37 @@ class Lab
         glutDestroyWindow(@window)
         exit 0
       # w, a, s, d: translate
-      when 119 # w
+      when ?w
         @shift_y += 0.05
-      when 97 # a
+      when ?a
         @shift_x -= 0.05
-      when 115  # s
+      when ?s
         @shift_y -= 0.05
-      when 100 # d
+      when ?d
         @shift_x += 0.05
-      # q, e: rotate
-      when 113 # q
-        @rotate -= 1
-      when 101 # e
-        @rotate += 1
+      # y, u, h, j, n, m: rotate
+      when ?y
+        @rotate_x -= 1
+      when ?u
+        @rotate_x += 1
+      when ?h
+        @rotate_y -= 1
+      when ?j
+        @rotate_y += 1
+      when ?n
+        @rotate_z -= 1
+      when ?m
+        @rotate_z += 1
       # i, o: zoom in/out
-      when 105
+      when ?i
         @zoom += 0.05
-      when 111
+      when ?o
         @zoom -= 0.05
+      # r, f: transformation
+      when ?r
+        @trans += 0.5
+      when ?f
+        @trans -= 0.5
       else
         p key
     end
@@ -128,8 +146,9 @@ class Lab
 end
 
 puts <<EOF
-  W, A, S, D : translate
-  Q, E       : rotate
-  I, O       : scale
+  W, A, S, D       : translate
+  Y, U, H, J, N, M : rotate
+  I, O             : scale
+  R, F             : transformation
 EOF
-Lab.new
+Lab.instance
